658644963a910ddd953e57dcdc2cfe3f28f956ea
rayankiani <rayankiani7@gmail.com>
2026-05-02 20:20:04 +0500

    Fix Vite build: add type=module and vite config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  publicDir: 'public',
+})
